4c1102946f90da909bce176af8ba0ae1de56124b
hhs0001 <heitorstein@gmail.com>
2023-08-24 14:09:34 -0300

    update página principal

--- app/page.tsx
+++ app/page.tsx
@@ -13,43 +13,30 @@ export default function Home() {
 	return (
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
 			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>Make&nbsp;</h1>
-				<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-				<br />
-				<h1 className={title()}>
-					websites regardless of your design experience.
-				</h1>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Beautiful, fast and modern React UI library.
-				</h2>
+				<h1 className={title()}>Um jeito&nbsp;</h1>
+				<h1 className={title({ color: "violet" })}>fácil </h1>
+				<h1 className={title()}>de ver a previsão do </h1>
+				<h1 className={title({ color: "violet" })}>tempo&nbsp;</h1>
 			</div>
 
 			<div className="flex gap-3">
 				<Link
 					isExternal
 					as={NextLink}
-					href={siteConfig.links.docs}
+					href={"https://github.com/freecorps/meteoro/wiki"}
 					className={buttonStyles({ color: "primary", radius: "full", variant: "shadow" })}
 				>
-					Documentation
+					Documentação
 				</Link>
 				<Link
 					isExternal
 					as={NextLink}
 					className={buttonStyles({ variant: "bordered", radius: "full" })}
-					href={siteConfig.links.github}
+					href={"https://github.com/freecorps/meteoro"}
 				>
 					<GithubIcon size={20} />
 					GitHub
 				</Link>
-			</div>
-
-			<div className="mt-8">
-				<Snippet hideSymbol hideCopyButton variant="flat">
-					<span>
-						Get started by editing <Code color="primary">app/page.tsx</Code>
-					</span>
-				</Snippet>
 			</div>
 		</section>
 	);
