b87e7d74c3d3579d67a285203fd87c94b37637b8
Heitor Stein <37478781+hhs0001@users.noreply.github.com>
2023-09-28 18:05:05 +0000

    update mapa

--- app/mapa/page.tsx
+++ app/mapa/page.tsx
@@ -1,11 +1,114 @@
 "use client"
 
-export default function Mapa() {
+import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { MapContainer, TileLayer, Marker, useMap, Popup} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+// @ts-ignore
+import L from 'leaflet';
+// @ts-ignore
+import { Icon } from 'leaflet';
+import ReactDOMServer from 'react-dom/server';
+import { BiMap } from "react-icons/bi"
+
+type ESP = {
+  _id: string;
+  lat: number;
+  long: number;
+  readings: {
+    temperatura: number;
+    umidade: number;
+    data: Date;
+  }[];
+};
+
+function calculateBounds(espData: ESP[]): [[number, number], [number, number]] | null {
+  if (espData.length === 0) return null;
+
+  const lats = espData.map(esp => esp.lat);
+  const lngs = espData.map(esp => esp.long);
+
+  return [
+    [Math.min(...lats), Math.min(...lngs)],
+    [Math.max(...lats), Math.max(...lngs)]
+  ];
+}
+
+function BoundsSetter({ bounds }: { bounds: [[number, number], [number, number]] | null }) {
+  const map = useMap();
+
+  useEffect(() => {
+    if (bounds) {
+      map.fitBounds(bounds);
+    }
+  }, [bounds, map]);
+
+  return null;
+}
+
+function createSvgIcon(svg: JSX.Element) {
+  const svgString = ReactDOMServer.renderToString(svg);
+  const iconUrl = new URL(`data:image/svg+xml;base64,${btoa(svgString)}`);
   
+  return new Icon({
+    iconUrl: iconUrl.href,
+    iconSize: [30, 30]
+  });
+}
+
+export default function Previsao() {
+  const [espData, setEspData] = useState<ESP[]>([]);
+
+  useEffect(() => {
+    const fetchEspData = async () => {
+      try {
+        toast.info('Buscando ESPs...');
+        const response = await fetch('/api/getIds');
+        const ids: string[] = await response.json();
+        toast.success('ESPs carregados com sucesso!');
+
+        // Now, fetch the data for each ESP
+        const espDataPromises = ids.map(id => fetch(`/api/getDados`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ id }),
+        }).then(res => res.json()));
+
+        const allEspData: ESP[] = await Promise.all(espDataPromises);
+        setEspData(allEspData);
+      } catch (error) {
+        toast.error('Erro ao buscar ESPs.');
+      }
+    };
+
+    fetchEspData();
+  }, []);
+
+  const bounds = calculateBounds(espData);
+
+  const beerIcon = createSvgIcon(<BiMap />);
+
+
 
   return (
     <div>
-      
+      <MapContainer style={{ width: '650px', height: '400px' }}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {espData.map(esp => (
+          <Marker key={esp._id} position={[esp.lat, esp.long]} icon={beerIcon}>
+            <Popup>
+              ESP: {esp._id}
+            </Popup>
+          </Marker>
+        ))}
+        <BoundsSetter bounds={bounds} />
+      </MapContainer>
     </div>
   );
 }
+
+
+
+
